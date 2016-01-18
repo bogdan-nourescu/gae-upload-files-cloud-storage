90dcf9856c278a52392d2ed16af99b686450c193
Bogdan <bogdan.nourescu@cbn-it.ro>
2016-01-18 13:11:08 +0200

    first commit

---
+++ src/main/java/ro/cbn/it/gae2/upload/GcsUtils.java
@@ -0,0 +1,87 @@
+package ro.cbn.it.gae2.upload;
+
+import com.google.appengine.tools.cloudstorage.*;
+import com.google.apphosting.api.ApiProxy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+
+public class GcsUtils {
+    /**
+     *
+     * @return the default bucket (appID-appspot.com)
+     */
+    public static String getDefaultBucket() {
+        return getAppId() + ".appspot.com";
+    }
+
+    public static void writeFile(String fileName, String content, String contentType) throws IOException {
+        GcsFilename gcsFileName = new GcsFilename(GcsUtils.getDefaultBucket(), fileName);
+        writeFile(gcsFileName, content, contentType);
+    }
+
+    public static void writeFile(GcsFilename gcsFileName, String content, String contentType) throws IOException {
+            writeFile(gcsFileName, content.getBytes("UTF-8"), contentType);
+    }
+
+    public static void writeFile(String fileName, byte[] content, String contentType) throws IOException {
+        GcsFilename gcsFileName = new GcsFilename(GcsUtils.getDefaultBucket(), fileName);
+        writeFile(gcsFileName, content, contentType);
+    }
+
+    public static void writeFile(GcsFilename gcsFileName, byte[] content, String contentType) throws IOException {
+            GcsFileOptions options = new GcsFileOptions.Builder().mimeType(contentType).contentEncoding("UTF-8").build();
+            GcsService gcsService = GcsServiceFactory.createGcsService();
+            GcsOutputChannel writeChannel;
+
+            writeChannel = gcsService.createOrReplace(gcsFileName, options);
+            writeChannel.write(ByteBuffer.wrap(content));
+            writeChannel.close();
+    }
+
+    public static String readFile(String fileName) throws IOException {
+        GcsFilename gcsFilename = getGcsFileName(fileName);
+        return readFile(gcsFilename);
+    }
+
+    public static String readFile(GcsFilename gcsFilename) throws IOException {
+            GcsService gcsService = GcsServiceFactory.createGcsService();
+            StringBuilder builder = new StringBuilder();
+            GcsInputChannel readChannel = gcsService.openPrefetchingReadChannel(gcsFilename, 0, 1024 * 1024);
+            BufferedReader reader = new BufferedReader(Channels.newReader(readChannel, "UTF8"));
+            String aux;
+            while ((aux = reader.readLine()) != null) {
+                builder.append(aux).append("\r\n");
+            }
+            reader.close();
+            return builder.toString();
+    }
+
+    public static boolean deleteFile(String fileName) throws IOException {
+        return deleteFile(getGcsFileName(fileName));
+    }
+
+    public static boolean deleteFile(GcsFilename gcsFileName) throws IOException {
+        GcsService gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
+            return gcsService.delete(gcsFileName);
+    }
+    /**
+     * @param fileName the name of the file to be used to to generate the GcsFilename<br />
+     *                 The fileName can contain / (to simulate folders)
+     * @return GcsFilename on the default bucket (appID-appspot.com)
+     */
+    public static GcsFilename getGcsFileName(String fileName) {
+        return new GcsFilename(GcsUtils.getDefaultBucket(), fileName);
+    }
+
+    /**
+     * @return the appID without the "s~" at the beginning
+     */
+    public static String getAppId() {
+        String result = ApiProxy.getCurrentEnvironment().getAppId();
+        //the app-id is similar to s~appid or eu~appid depending if the app is in US or Europe
+        return result.substring(result.indexOf("~") + 1);
+    }
+}
---
+++ src/main/java/ro/cbn/it/gae2/upload/UploadFile.java
@@ -0,0 +1,54 @@
+package ro.cbn.it.gae2.upload;
+
+import com.google.appengine.tools.cloudstorage.*;
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/upload")
+@MultipartConfig
+public class UploadFile extends HttpServlet{
+    private final GcsService gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Logger logger = Logger.getLogger(this.getClass().getName());
+        logger.log(Level.SEVERE,"entered");
+
+
+        String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
+        logger.log(Level.SEVERE,"description: "+description);
+
+        Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+        String fileName = filePart.getSubmittedFileName();
+        InputStream fileContent = filePart.getInputStream();
+        logger.log(Level.SEVERE,"file: "+fileName);
+        // ... (do your job here)
+        GcsFileOptions fileOptions = new GcsFileOptions.Builder().mimeType("application/octet-stream").contentEncoding("UTF-8").build();
+        GcsOutputChannel outputChannel = gcsService.createOrReplace(getFileName(fileName), fileOptions);
+        IOUtils.copy(fileContent, Channels.newOutputStream(outputChannel));
+        fileContent.close();
+        outputChannel.close();
+    }
+
+    public GcsFilename getFileName(String fileName){
+        //the recommandation is to create an unique filename.
+        //one way to do this is to create an Entity that contains the file meta and use the id/hash to uniquely identify the file
+        return GcsUtils.getGcsFileName(fileName);
+    }
+}
