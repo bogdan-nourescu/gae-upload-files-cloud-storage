2062b67fa3297f3c56f518c1c8d3dff4ce2ff8b4
Bogdan <bogdan.nourescu@cbn-it.ro>
2016-01-18 17:14:16 +0200

    added a test servlet that downloads over 32MB of data as a file. On managed VM does not work

---
+++ src/main/java/ro/cbn/it/gae2/download/DownloadBigFile.java
@@ -0,0 +1,30 @@
+package ro.cbn.it.gae2.download;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/download_over32mb")
+@MultipartConfig
+public class DownloadBigFile extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setHeader("Content-disposition","attachment; filename=over32mb.txt");
+        for (int i = 0; i < 1024*1024*2; i++) {
+            out.print("col1,col2,col3,col4,col5,col6,col6\r\n");
+        }
+        out.close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req,resp);
+    }
+
+}
